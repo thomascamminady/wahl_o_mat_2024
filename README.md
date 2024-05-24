07daaf4ae311d428f0370278c8771d531f0d46ac
Thomas Camminady <0milieux_member@icloud.com>
2024-05-24 10:08:16 +0200

    Add image for embedding.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ See `wahl-o-mat-2024.csv` for the answers of all German parties to the Wahl-O-Ma
 Run `poetry run python wahl_o_mat_2024/parse.py` to recreate results.
 
 The `module_definition.js` file is taken from the official Wahl-O-Mat website on 2024/05/22, 5PM.
+
+![Embedding of parties](embedding.png)
--- wahl_o_mat_2024/analyze.py
+++ wahl_o_mat_2024/analyze.py
@@ -1,6 +1,14 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import polars as pl
 from sklearn.manifold import TSNE as EMBEDDING
+
+
+def metric(x, y):
+    # return np.sum(np.abs(x - y))
+    # return np.sum(x != y)
+    return np.linalg.norm(x - y)
+
 
 if __name__ == "__main__":
     df = pl.read_csv("wahl-o-mat-2024.csv").drop("question", "question_id")
@@ -10,10 +18,11 @@ if __name__ == "__main__":
     embedding = EMBEDDING(metric="euclidean")
     _ = embedding.fit_transform(X)
     x, y = _[:, 0], _[:, 1]
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(8, 8))
     ax.scatter(x, y)
     if not len(x) == len(y) or not len(x) == len(df.columns):
         raise ValueError("Wrong dimensions")
     for xi, yi, ci in zip(x, y, df.columns):
-        ax.text(xi, yi, ci)
-    plt.show()
+        ax.text(xi, yi + 0.02, ci, ha="center")
+    plt.axis("off")
+    plt.savefig("embedding.png")
